f706fb8cf050907e348774225421a637b319faa4
RasoulJf <rasouljalilian8@gmail.com>
2025-02-03 23:05:52 +0330

    Complete CreateCategory

--- BackEnd/Controllers/categoryCn.js
+++ BackEnd/Controllers/categoryCn.js
@@ -1,9 +1,20 @@
-import catchAsync from "../Utils/catchAsync.js"
-import Category from "../Models/categoryMd.js"
-export const createCategory=catchAsync(async(req,res,next)=>{})
-export const getAllCategory=catchAsync(async(req,res,next)=>{})
-export const getOneCategory=catchAsync(async(req,res,next)=>{})
-export const updateCategory=catchAsync(async(req,res,next)=>{})
-export const removeCategory=catchAsync(async(req,res,next)=>{})
-
-
+import catchAsync from "../Utils/catchAsync.js";
+import jwt from 'jsonwebtoken'
+import Category from "../Models/categoryMd.js";
+export const createCategory = catchAsync(async (req, res, next) => {
+  const { id } = jwt.verify(
+    req?.headers?.authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
+  const {userId,...others}=req.body
+  const category = await Category.create({userId:id,...others});
+  return res.status(200).json({
+    category,
+    message:"Category Created Successfully",
+    success:true
+  })
+});
+export const getAllCategory = catchAsync(async (req, res, next) => {});
+export const getOneCategory = catchAsync(async (req, res, next) => {});
+export const updateCategory = catchAsync(async (req, res, next) => {});
+export const removeCategory = catchAsync(async (req, res, next) => {});
--- BackEnd/Routes/category.js
+++ BackEnd/Routes/category.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { createCategory,getOneCategory,getAllCategory,updateCategory,removeCategory } from '../Controllers/categoryCn.js'
+import isLogin from '../MiddleWare/isLogin.js';
 const categoryRouter = express.Router()
-categoryRouter.route("/").post(createCategory).get(getAllCategory);
+categoryRouter.route("/").post(isLogin,createCategory).get(getAllCategory);
 categoryRouter.route("/:id").get(getOneCategory).patch(updateCategory).delete(removeCategory);
 export default categoryRouter
