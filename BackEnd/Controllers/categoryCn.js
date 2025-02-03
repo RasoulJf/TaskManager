03d49e28dbdac1c39b4b3f317ff849937414d2c3
RasoulJf <rasouljalilian8@gmail.com>
2025-02-03 23:45:31 +0330

    Complete GetCategories

--- BackEnd/Controllers/categoryCn.js
+++ BackEnd/Controllers/categoryCn.js
@@ -1,6 +1,7 @@
 import catchAsync from "../Utils/catchAsync.js";
 import jwt from 'jsonwebtoken'
 import Category from "../Models/categoryMd.js";
+import ApiFeatures from "../Utils/apiFeatures.js";
 export const createCategory = catchAsync(async (req, res, next) => {
   const { id } = jwt.verify(
     req?.headers?.authorization.split(" ")[1],
@@ -14,7 +15,26 @@ export const createCategory = catchAsync(async (req, res, next) => {
     success:true
   })
 });
-export const getAllCategory = catchAsync(async (req, res, next) => {});
+export const getAllCategory = catchAsync(async (req, res, next) => {
+    const { id:userId } = jwt.verify(
+        req?.headers?.authorization.split(" ")[1],
+        process.env.JWT_SECRET
+      );
+      const filters = req.query.filters ? {...req.query.filters,userId} : {userId}
+      const queryString = {...req.query,filters}
+      const features = new ApiFeatures(Category,queryString).filter().paginate().sort().limitFields().search()
+      const categories = await features.query.populate({
+        path:"userId",
+        select:"username email"
+      })
+      const countCategories = await Category.countDocuments(filters)
+      return res.status(200).json({
+        categories,
+        countCategories,
+        message:"Get Categories Successfully",
+        success:true
+      })
+});
 export const getOneCategory = catchAsync(async (req, res, next) => {});
 export const updateCategory = catchAsync(async (req, res, next) => {});
 export const removeCategory = catchAsync(async (req, res, next) => {});
