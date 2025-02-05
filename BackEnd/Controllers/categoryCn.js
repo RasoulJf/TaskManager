2430cf0b9ced1b8f9e080dcdb58cd57a68c4603e
RasoulJf <rasouljalilian8@gmail.com>
2025-02-05 08:37:03 +0330

    Update categoryCn.js

--- BackEnd/Controllers/categoryCn.js
+++ BackEnd/Controllers/categoryCn.js
@@ -4,95 +4,53 @@ import Category from "../Models/categoryMd.js";
 import ApiFeatures from "../Utils/apiFeatures.js";
 import HandleERROR from "../Utils/handleError.js";
 export const createCategory = catchAsync(async (req, res, next) => {
-  const { id } = jwt.verify(
-    req?.headers?.authorization.split(" ")[1],
-    process.env.JWT_SECRET
-  );
-  const {userId,...others}=req.body
-  const category = await Category.create({userId:id,...others});
-  return res.status(200).json({
-    category,
+  const category = await Category.create({...req.body, userId:req.userId});
+  return res.status(201).json({
+    data:category,
     message:"Category Created Successfully",
     success:true
   })
 });
 export const getAllCategory = catchAsync(async (req, res, next) => {
-    const { id:userId } = jwt.verify(
-        req?.headers?.authorization.split(" ")[1],
-        process.env.JWT_SECRET
-      );
-      const filters = req.query.filters ? {...req.query.filters,userId} : {userId}
-      const queryString = {...req.query,filters}
-      const features = new ApiFeatures(Category,queryString).filter().paginate().sort().limitFields().search()
-      const categories = await features.query.populate({
-        path:"userId",
-        select:"username email"
-      })
-      const countCategories = await Category.countDocuments(filters)
+      const queryString = {...req.query,filters:{...req.query.fiters,userId:req.userId}}
+      const features = new ApiFeatures(Category,queryString).filter().paginate().sort().limitFields().search().populate()
+      const categories = await features.query
       return res.status(200).json({
-        categories,
-        countCategories,
+        data:categories,
         message:"Get Categories Successfully",
         success:true
       })
 });
 export const getOneCategory = catchAsync(async (req, res, next) => {
-  const { id:userId } = jwt.verify(
-    req?.headers?.authorization.split(" ")[1],
-    process.env.JWT_SECRET
-  );
-  const {id:categoryId}=req.params
-  const category = await Category.findOne({_id:categoryId,userId}).populate(
-    {
-      path:"userId",
-        select:"username email"
-    }
-  )
-  if(!category){
-    return next(new HandleERROR('you dont have permission',400))
-  }
+  const {id}=req.params
+  const category = await Category.findOne({_id:id,userId:req.userId})
   return res.status(200).json({
-    category,
+    data:category,
     message:"Get Category Successfully",
     success:true
   })
 });
 export const updateCategory = catchAsync(async (req, res, next) => {
-  const { id:userId } = jwt.verify(
-    req?.headers?.authorization.split(" ")[1],
-    process.env.JWT_SECRET
-  );
-  const {id:categoryId}=req.params
-const newCategory = await Category.findByIdAndDelete({
-  _id: categoryId,
-  userId
-}
-)
-if(!newCategory){
-  return next(new HandleERROR('you dont have permission',400))
+  const {id}=req.params
+const category = await Category.findByIdAndUpdate({_id: id,userId:req.userId},req.body,{new:true , runValidators:true})
+if(!category){
+  return next(new HandleERROR('Category Not Found',404))
 }
 return res.status(200).json({
-  newCategory,
+  data:category,
   message:"Update Category Successfully",
   success:true
 })
 });
 export const removeCategory = catchAsync(async (req, res, next) => {
-  const { id:userId } = jwt.verify(
-    req?.headers?.authorization.split(" ")[1],
-    process.env.JWT_SECRET
-  );
-  const {id:categoryId}=req.params
-const category = await Category.findByIdAndDelete({
-  _id: categoryId,
-  userId
-}
-)
+  const {id}=req.params
+const category = await Category.findByIdAndDelete({_id: id,userId:req.userId})
 if(!category){
-  return next(new HandleERROR('you dont have permission',400))
+  return next(new HandleERROR('Category Not Found',404))
 }
 return res.status(200).json({
-  message:"Delete Category Successfully",
+  data:category,
+  message:"Update Category Successfully",
   success:true
 })
 });
