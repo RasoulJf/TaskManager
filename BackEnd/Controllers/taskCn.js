910371cd440b2a74ae0f4b03faa24654be71304f
RasoulJf <rasouljalilian8@gmail.com>
2025-02-05 08:40:48 +0330

    Complete Create Task

--- BackEnd/Controllers/taskCn.js
+++ BackEnd/Controllers/taskCn.js
@@ -1,6 +1,13 @@
 import catchAsync from "../Utils/catchAsync.js"
 import Task from "../Models/taskMd.js"
-export const createTask=catchAsync(async(req,res,next)=>{})
+export const createTask=catchAsync(async(req,res,next)=>{
+    const task = await Task.create({...req.body, userId:req.userId});
+    return res.status(201).json({
+      data:task,
+      message:"Task Created Successfully",
+      success:true
+    })
+})
 export const getAllTasks=catchAsync(async(req,res,next)=>{})
 export const getOneTask=catchAsync(async(req,res,next)=>{})
 export const updateTask=catchAsync(async(req,res,next)=>{})
