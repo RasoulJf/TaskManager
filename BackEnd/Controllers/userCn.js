4f4f659f3acd35fa32f47f36f8c25c8fb380e45b
RasoulJf <rasouljalilian8@gmail.com>
2025-02-07 19:17:22 +0330

    Update userCn.js

--- BackEnd/Controllers/userCn.js
+++ BackEnd/Controllers/userCn.js
@@ -1,18 +1,18 @@
 import catchAsync from "../Utils/catchAsync.js"
 import User from "../Models/userMd.js"
 export const updateUser=catchAsync(async(req,res,next)=>{
-    const user = await User.findById(req.userId)
-    return res.status(200).jaon({
-        success:true,
-        message:"I Get You :)",
-        data:user
-    })
-})
-export const getOneUser=catchAsync(async(req,res,next)=>{
     const user=await User.findByIdAndUpdate(req.userId,req.body,{new:true,runValidators:ture})
     return res.status(200).json({
         success:true,
         message:"I Change You :)", 
+        data:user
+    })
+})
+export const getOneUser=catchAsync(async(req,res,next)=>{
+    const user = await User.findById(req.userId)
+    return res.status(200).jaon({
+        success:true,
+        message:"I Get You :)",
         data:user
     })
 })
